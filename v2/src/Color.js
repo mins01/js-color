6ac28b57f2bac18d2f17cd3ec85a20b70de7a1cd
mins01 <mins01@inuscomm.co.kr>
2026-03-31 10:34:49 +0900

    toXxx 는 isReal 제거, 무조건 실수형으로 계산

--- v2/src/Color.js
+++ v2/src/Color.js
@@ -246,62 +246,47 @@ export default class Color{
   toRgba(){ return { ...this.toRgb(), a: this.a}; }
   toRealRgb(){ return { r: this.realR, g: this.realG, b: this.realB}; }
   toRealRgba(){ return { ...this.toRealRgb(), a: this.a}; }
-  toHsl(isReal=true){
-    if(!isReal){
-      if(!this.#cache.has('hsl')) this.#cache.set('hsl',rgbToHsl(this.r, this.g, this.b));
-      return this.#cache.get('hsl');
-    }else{
-      if(!this.#cache.has('real-hsl')) this.#cache.set('real-hsl',rgbToHsl(this.realR, this.realG, this.realB));
-      return this.#cache.get('real-hsl');
-    }
+  toHsl(){
+    if(!this.#cache.has('hsl')) this.#cache.set('hsl',rgbToHsl(this.realR, this.realG, this.realB));
+    return this.#cache.get('hsl');
   }
-  toHsla(isReal=true){ return {...this.toHsl(isReal),a:this.a}; }
+  toHsla(){ return {...this.toHsl(),a:this.a}; }
   toHslString() {
-    const { h, s, l } = this.toHsl(false);
+    const { h, s, l } = this.toHsl();
     return `hsl(${this.toFixed(h,0)}, ${this.toFixed(s*100,0)}%, ${this.toFixed(l*100,0)}%)`;
   }
   toHslaString(){
-    const { h, s, l , a} = this.toHsla(false);
+    const { h, s, l , a} = this.toHsla();
     return `hsla(${this.toFixed(h,0)}, ${this.toFixed(s*100,0)}%, ${this.toFixed(l*100,0)}%, ${a.toFixed(3)})`;
   }
-  toHsv(isReal=true){
-    if(!isReal){
-      if(!this.#cache.has('hsv')) this.#cache.set('hsv',rgbToHsv(this.r, this.g, this.b));
-      return this.#cache.get('hsv');
-    }else{
-      if(!this.#cache.has('real-hsv')) this.#cache.set('real-hsv',rgbToHsv(this.realR, this.realG, this.realB));
-      return this.#cache.get('real-hsv');
-    }
+  toHsv(){
+    if(!this.#cache.has('hsv')) this.#cache.set('hsv',rgbToHsv(this.realR, this.realG, this.realB));
+    return this.#cache.get('hsv');
   }
-  toHsva(isReal=true){ return {...this.toHsv(isReal),a:this.a}; }
+  toHsva(){ return {...this.toHsv(),a:this.a}; }
   toHsvString() {
-    const { h, s, v } = this.toHsv(false);
+    const { h, s, v } = this.toHsv();
     return `hsv(${this.toFixed(h,0)}, ${this.toFixed(s*100,0)}%, ${this.toFixed(v*100,0)}%)`;
   }
   toHsvaString() {
-    const { h, s, v , a} = this.toHsva(false);
+    const { h, s, v , a} = this.toHsva();
     return `hsva(${this.toFixed(h,0)}, ${this.toFixed(s*100,0)}%, ${this.toFixed(v*100,0)}%, ${a.toFixed(3)})`;
   }
-  toHsb(isReal=true){ const {h,s,v} = this.toHsv(isReal); return {h,s,b:v}; }
-  toHsba(isReal=true){ return {...this.toHsb(isReal),a:this.a}; }
+  toHsb(){ const {h,s,v} = this.toHsv(); return {h,s,b:v}; }
+  toHsba(){ return {...this.toHsb(),a:this.a}; }
   toHsbString() {
-    const { h, s, b } = this.toHsb(false);    
+    const { h, s, b } = this.toHsb();    
     return `hsb(${this.toFixed(h,0)}, ${this.toFixed(s*100,0)}%, ${this.toFixed(b*100,0)}%)`;
   }
   toHsbaString() {
-    const { h, s, b, a } = this.toHsba(false);
+    const { h, s, b, a } = this.toHsba();
     return `hsba(${this.toFixed(h,0)}, ${this.toFixed(s*100,0)}%, ${this.toFixed(b*100,0)}%, ${a.toFixed(3)})`;
   }
-  toCmyk(isReal=true){
-    if(!isReal){
-      if(!this.#cache.has('cmyk')) this.#cache.set('cmyk',rgbToCmyk(this.r, this.g, this.b));
-      return this.#cache.get('cmyk');
-    }else{
-      if(!this.#cache.has('cmyk')) this.#cache.set('cmyk',rgbToCmyk(this.realR, this.realG, this.realB));
-      return this.#cache.get('cmyk');  
-    }
+  toCmyk(){
+    if(!this.#cache.has('cmyk')) this.#cache.set('cmyk',rgbToCmyk(this.realR, this.realG, this.realB));
+    return this.#cache.get('cmyk');
   }
-  toCmyka(isReal=true){ return {...this.toCmyk(isReal),a:this.a}; }
+  toCmyka(){ return {...this.toCmyk(),a:this.a}; }
   toCmykString() {
     const { c, m, y, k } = this.toCmyk();
     const pct = v => Math.round(v * 100);
--- v2/test/test.testColor.js
+++ v2/test/test.testColor.js
@@ -12,23 +12,69 @@ const assert = (label, result, expected) => {
 };
 
 // === constructor ===
+// {
+//   // const h = 106.73076923076923
+//   // const s = 0.7536231884057971
+//   // const l = 0.27058823529411763
+//   const h = 108
+//   const s = 0.7953125
+//   const l = 0.14218750000000002
+
+
+//   console.log({h,s,l});
+//   const c = new Color();
+//   c.setHsla(h,s,l)
+//   const hsl = c.toHsl()
+//   console.log('diff',hsl.h-h,hsl.s-s,hsl.l-l)
+//   console.log(c.toRgb())
+//   console.log(c.toRealRgb())
+//   console.log(c.toHsl())
+
+
+// }
+// {
+//   let h = 108
+//   let s = 0.3;
+//   let l = 0.4;
+//   const c = new Color();
+//   for(let i=0,m=20;i<m;i++){
+//     h = Math.random()*360;
+//     s = Math.random();
+//     l = Math.random();
+//     c.setHsl(h,s,l);
+//     // console.log(c.toRealRgb(),c.toHsl(),c.toHsl(false));
+//     assert(`hsl-${i}`,c.toHsl(),{h,s,l})
+//   }
+// }
+// {
+//   let h = 108
+//   let s = 0.3;
+//   let l = 0.4;
+//   const c = new Color();
+//   for(let i=0,m=20;i<m;i++){
+//     h = Math.random()*360;
+//     s = Math.random();
+//     l = Math.random();
+//     let hslString =`hsl(${Math.round(h)}, ${Math.round(s*100)}%, ${Math.round(l*100)}%)` 
+//     c.setString(hslString);
+//     // console.log(c.toRealRgb(),c.toHsl(),c.toHsl(false));
+//     assert(`hsl-${i}`,c.toHslString(),hslString)
+//   }
+// }
 {
-  // const h = 106.73076923076923
-  // const s = 0.7536231884057971
-  // const l = 0.27058823529411763
-  const h = 108
-  const s = 0.7953125
-  const l = 0.14218750000000002
+  // expected: "hsl(318, 0%, 51%)"
+  // got:      "hsl(0, 0%, 51%)"
 
-
-  console.log({h,s,l});
   const c = new Color();
-  c.setHsla(h,s,l)
-  const hsl = c.toHsl()
-  console.log('diff',hsl.h-h,hsl.s-s,hsl.l-l)
-  console.log(c.toRgb())
-  console.log(c.toRealRgb())
-  console.log(c.toHsl())
-
-
+  let i = 0;
+  let hslString =`hsl(318, 34%, 99.99%)` 
+  c.setString(hslString);
+  console.log(c.toRealRgb(),c.toHsl(),c.toHsl(false));
+  assert(`hsl-${i}`,c.toHslString(),hslString)
+  c.setHsl(318,0.34,0.9999);
+  console.log(c.toRealRgb(),c.toHsl(),c.toHsl(false));
+  
 }
+
+
+ 
